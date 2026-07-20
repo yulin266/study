cf5d2d8839e8747035409e5cd4cfbd3dd0a3b964
yulin266 <2664107253@qq.com>
2026-07-20 21:28:04 +0800

    feat: day01

--- HelloTest.java
+++ HelloTest.java
@@ -1,5 +1,5 @@
 public class HelloTest {
     public static void main (String args){
-        System.out.println("第一天的学习" );
+        System.out.println("第一天的学习");
     }
 }
---
+++ HelloTest2.java
@@ -0,0 +1,4 @@
+public class HelloTest2 {  public static void main (String args){
+    System.out.println("第一天的学习");
+}
+}
