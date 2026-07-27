c02ea1fe79c8e7d0341160909548e2377242b2b8
yulin266 <2664107253@qq.com>
2026-07-27 21:14:07 +0800

    更新笔记和代码

--- coding/Day00.java
+++ coding/Day00.java
@@ -2,7 +2,7 @@
 package coding;
 import java.util.Scanner;
 
-public class Day01 {
+public class Day00 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
